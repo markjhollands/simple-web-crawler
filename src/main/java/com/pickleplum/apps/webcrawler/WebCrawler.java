e9e3ee271648e25ffe0a9d89c04a1019dfdb4f55
Mark Hollands <markjhollands@gmail.com>
2019-07-12 19:46:19 +0100

    Updated POM to build with dependencies

--- src/main/java/com/pickleplum/apps/webcrawler/WebCrawler.java
+++ src/main/java/com/pickleplum/apps/webcrawler/WebCrawler.java
@@ -1,11 +1,21 @@
 /*
+ * Web Crawler application
+ * 
  * Copyright (c) 2019 Mark Hollands
+ * 
+ * Simple web crawler. Given a URL the application will crawl
+ * each page, retrieve all links and follow local links.
+ * 
+ * The application will not crawl the same link twice, as this
+ * could cause an infinite loop.
+ * 
  */
 
 package com.pickleplum.apps.webcrawler;
 
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,19 +23,16 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.ConnectException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 /*
+ * WebCrawler class
+ * 
+ * Main class for WebCrawler application
+ * 
  * @author Mark Hollands
- * 
- * Web Crawler application
- * 
- * Simple web crawler. Given a URL the application will crawl
- * each page, retrieve all links and follow local links.
- * 
- * The application will not crawl the same link twice, as this
- * could cause an infinite loop.
  */
 public class WebCrawler {
 	
@@ -87,33 +94,40 @@ public class WebCrawler {
 	 */
 	private void crawl(String sep, String url) throws IOException {
 		if (!links.contains(url)) {
-        	URL rawURL = new URL(url);
-        	if (rawURL.getHost().contentEquals(hostname) && rawURL.getPort() == port) {
-        		links.add(url);
-        		
-        		Document doc = null;
-        		try {
-        			doc = Jsoup.connect(url).get();
-        		} catch (ConnectException ce) {
-        			printLink(sep, url, "Unable to connect to link, cannot crawl");
-        		} catch (HttpStatusException hse) {
-        			if (hse.getStatusCode() == 404) {
-        				printLink(sep, url, "404 - Link not found, cannot crawl");
-        			} else {
-        				printLink(sep, url, "Unable to connect to link, cannot crawl");
-        			}
-        		}
-        		
-        		if (doc != null) {
-        			Elements links = doc.select("a[href]");
-        			printLink(sep, url, String.format("Links: %d", links.size()));
-    		        for (Element link : links) {
-    		            crawl("  " + sep, link.attr("abs:href"));
-    		        }
-        		}
-        	} else {
-        		printLink(sep, url, "External link, will not crawl");
-        	}
+			try {
+				URL rawURL = new URL(url);
+				
+				if (rawURL.getHost().contentEquals(hostname) && rawURL.getPort() == port) {
+	        		links.add(url);
+	        		
+	        		Document doc = null;
+	        		try {
+	        			doc = Jsoup.connect(url).get();
+	        		} catch (ConnectException ce) {
+	        			printLink(sep, url, "Unable to connect to link, cannot crawl");
+	        		} catch (HttpStatusException hse) {
+	        			if (hse.getStatusCode() == 404) {
+	        				printLink(sep, url, "404 - Link not found, cannot crawl");
+	        			} else {
+	        				printLink(sep, url, "Unable to connect to link, cannot crawl");
+	        			}
+	        		} catch (UnsupportedMimeTypeException umte) {
+	        			printLink(sep, url, "Unable to connect to link, unsupported MIME type");
+	        		}
+	        		
+	        		if (doc != null) {
+	        			Elements links = doc.select("a[href]");
+	        			printLink(sep, url, String.format("Links: %d", links.size()));
+	    		        for (Element link : links) {
+	    		            crawl("  " + sep, link.attr("abs:href"));
+	    		        }
+	        		}
+	        	} else {
+	        		printLink(sep, url, "External link, will not crawl");
+	        	}
+			} catch (MalformedURLException mue) {
+    			printLink(sep, url, "Unable to connect to link, malformed URL");
+    		}
 		} else {
 			printLink(sep, url, "Already crawled");
 		}
